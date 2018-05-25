941dd8c6ef186c5cb314567c624b9c5d552a5297
UCTRONICS <avahe@uctronics.com>
2018-05-25 10:00:33 +0800

    Update remoteserver.c

--- C/src/remoteserver.c
+++ C/src/remoteserver.c
@@ -1084,7 +1084,7 @@ void  INThandler(int sig)
 {
      char  c;
      signal(sig, SIG_IGN);
-     printf("OUCH, did you hit Ctrl-C?\n"
+     printf("You hit Ctrl-C?\n"
             "Do you really want to quit? [y/n] ");
      c = getchar();
      if (c == 'y' || c == 'Y'){
