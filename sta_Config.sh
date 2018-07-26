c6855ca3c0119632b334dfd9205d36411fea6b33
UCTRONICS <avahe@uctronics.com>
2018-07-26 17:12:27 +0800

    Update sta_Config.sh

--- sta_Config.sh
+++ sta_Config.sh
@@ -3,3 +3,4 @@ sudo cp wifiConfig/sta_dhcpcd.conf /etc/dhcpcd.conf
 sudo cp wifiConfig/sta_dnsmasq.conf /etc/dnsmasq.conf
 echo "STA mode configure OK, please run 'sudo ifconfig' to check the ip address"
 echo "Please 'run sudo reboot' to reboot"
+sudo reboot
