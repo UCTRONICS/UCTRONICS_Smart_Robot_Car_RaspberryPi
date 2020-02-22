bcb36c981101b8c54b6d9caae427a34928965e0b
arducam <admin@arducam.com>
2020-02-22 18:01:12 +0800

    Update rpihw.c

--- C/src/rpihw.c
+++ C/src/rpihw.c
@@ -353,7 +353,15 @@ static const rpi_hw_t rpi_hw_info[] = {
         .osc_freq = OSC_FREQ_Pi4,
         .desc = "Pi 4",
     },
-
+     //Pi 4B
+    {
+        .hwver  = 0xA03111,
+        .type = RPI_HWVER_TYPE_PI4,
+        .periph_base = PERIPH_BASE_RPI4,
+        .videocore_base = VIDEOCORE_BASE_RPI4,
+        .osc_freq = OSC_FREQ_Pi4,
+        .desc = "Pi 4",
+    },
 
 };
 
